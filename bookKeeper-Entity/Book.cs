75938b4a16821ab9ab967d26dc21a8f681e7412d
Kate Kasiutich <kitkat.katekass@gmail.com>
2020-05-23 21:51:20 +0300

    Add project files.

---
+++ bookKeeper-Entity/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bookKeeper_Entity
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public User UserId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+    }
+}
