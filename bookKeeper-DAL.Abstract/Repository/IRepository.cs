8dea8e87f23851d10ada1b0a75fbfbf23993f8b9
Kate Kasiutich <kitkat.katekass@gmail.com>
2020-05-23 22:56:48 +0300

    добавлен универсльный репозиторий, а также интефесы для классов User, Book

---
+++ bookKeeper-DAL.Abstract/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bookKeeper_DAL.Abstract.Repository
+{
+    interface IRepository<TKey, TEntity> : IDisposable where TKey : struct where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll(); 
+        TEntity GetSingle(TKey id);
+        void Create(TEntity item);
+        void Update(TEntity item);
+        void Delete(TKey id);
+        void Save();
+    }
+}
