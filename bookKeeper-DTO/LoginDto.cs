79f33a402931928cde8eb6ac3fd77fde73b2afb1
Kate Kasiutich <kitkat.katekass@gmail.com>
2020-05-25 14:37:46 +0300

    LoginDto & AuthDto

--- bookKeeper-DTO/LoginDto.cs
+++ bookKeeper-DTO/LoginDto.cs
@@ -6,11 +6,8 @@ using System.Web;
 
 namespace bookKeeper_DTO
 {
-    public class AuthDto
+    public class LoginDto
     {
-        [Required(ErrorMessage = "Не указано Name")]
-        public string Name { get; set; }
-
         [Required(ErrorMessage = "Не указан Email")]
         public string Email { get; set; }
 
@@ -18,16 +15,10 @@ namespace bookKeeper_DTO
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [DataType(DataType.Password)]
-        [Compare("Password", ErrorMessage = "Пароль введен неверно")]
-        public string ConfirmPassword { get; set; }
-
-        public AuthDto(string email, string password, string confirmPassword, string name)
+        public LoginDto(string email, string password)
         {
             Email = email;
             Password = password;
-            ConfirmPassword = confirmPassword;
-            Name = name;
         }
     }
 }
--- bookKeeper/Startup.cs
+++ bookKeeper/Startup.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Owin;
+using Microsoft.AspNetCore.Builder;
 
 [assembly: OwinStartup(typeof(bookKeeper.Startup))]
 
