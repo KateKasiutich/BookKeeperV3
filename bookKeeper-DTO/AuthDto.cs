d9d9a3b53776ba5dc0c16a8d3507b3a9b604b2fd
Kate Kasiutich <kitkat.katekass@gmail.com>
2020-05-25 13:07:30 +0300

    AuthDto added

---
+++ bookKeeper-DTO/AuthDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bookKeeper_DTO
+{
+    public class AuthDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public AuthDto(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+    }
+}
---
+++ bookKeeper/IoC.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace bookKeeper
+{
+    public static class IoC
+    {
+        private static readonly IDictionary<Type, Type> Registered = new Dictionary<Type, Type>();
+
+        public static void Register<TType, TConcrete>() where TConcrete : class, TType
+        {
+            Registered[typeof(TType)] = typeof(TConcrete);
+        }
+
+        public static TTypeToResolve Resolve<TTypeToResolve>()
+        {
+            return (TTypeToResolve)Resolve(typeof(TTypeToResolve));
+        }
+
+        public static object Resolve(Type type)
+        {
+            return GetInstance(Registered[type]);
+        }
+
+        private static object GetInstance(Type type)
+        {
+            var parameters = ResolveConstructorParameters(type);
+            return Activator.CreateInstance(type, parameters.ToArray());
+        }
+
+        private static IEnumerable<object> ResolveConstructorParameters(Type type)
+        {
+            var constructorInfo = type.GetConstructors().First();
+            return constructorInfo.GetParameters().Select(parameter => Resolve(parameter.ParameterType));
+        }
+    }
+}
