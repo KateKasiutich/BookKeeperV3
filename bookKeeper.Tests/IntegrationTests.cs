3e09477b2ce65b398971c4b5848dcf4b7922bd25
Kate Kasiutich <kitkat.katekass@gmail.com>
2020-05-31 13:17:32 +0300

    integration tests done

--- bookKeeper.Tests/IntegrationTests.cs
+++ bookKeeper.Tests/IntegrationTests.cs
@@ -73,6 +73,44 @@ namespace bookKeeper.Tests
             //Act
             bookService.AddBook(bookDto, wrongUserId);
         }
+
+        [TestMethod]
+        public void AddUser_NormalData_Success()
+        {
+            //Arrange
+            var userRepo = IoC.Resolve<IUserRepository>();
+            var user = new User();
+            user.Email = "email";
+            user.Name = "Name";
+            user.Password = "password";
+
+            //Act
+            userRepo.Create(user);
+            userRepo.Save();
+
+            //Assert
+            var context = IoC.Resolve<DataBaseContext>();
+            var getUser = context.Users.First(u => u.Name == user.Name);
+            Assert.AreEqual(user.Name, getUser.Name);
+            Assert.AreEqual(user.Password, getUser.Password);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DbUpdateException))]
+        public void AddUser_NullName_Sql()
+        {
+            //Arrange
+            var userRepo = IoC.Resolve<IUserRepository>();
+
+            var user1 = new User() { Email = "test@email.com", Id = 0, Name = null, Password = "Pa$$w0rd" };
+
+
+            //Act
+            userRepo.Create(user1);
+            userRepo.Save();
+        }
+
+
     }
 }
 
